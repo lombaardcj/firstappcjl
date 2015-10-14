e83b9ffad90f237cc3943860d3ca1f3f6d21d478
Chris Lombaard <lombaardcj@gmail.com>
2015-10-15 00:31:22 +0200

    Changed the HTML for the site by adding new view show_projects for Timekeeper App

---
+++ timekeeper/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.show_projects, name='show_project_list'),
+]
--- timekeeper/views.py
+++ timekeeper/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def show_projects(request):
+	return render(request, 'timekeeper/show_project_list.html', {})
