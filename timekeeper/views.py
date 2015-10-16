e7aecc44a7d8ff08c9c32f613d9246f55044d656
Chris Lombaard <lombaardcj@gmail.com>
2015-10-16 22:17:20 +0200

    Modified template to view all customers from database.

--- timekeeper/views.py
+++ timekeeper/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import Customer
+
 def show_projects(request):
-	return render(request, 'timekeeper/show_project_list.html', {})
+	customers = Customer.objects.all()
+	
+	return render(request, 'timekeeper/show_project_list.html', {'customers':customers})
